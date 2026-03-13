9093f82715d4441d757649a39ebb96fa73b175d1
sumonali940 <isumon105@gmail.com>
2026-03-13 13:27:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Countries from './component/Countries/Countries'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>React World on the go...</h1>
+      <Countries></Countries>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h4>In the Country</h4>
+        </div>
+    );
+};
+
+export default Countries;
